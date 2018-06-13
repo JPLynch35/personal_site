58a8030147191785dfa27ef58b664d43d618e5ba
JP Lynch <JPLynch35@gmail.com>
2018-06-13 12:27:28 -0600

    adds background color

--- test/features/user_sees_a_homepage_test.rb
+++ test/features/user_sees_a_homepage_test.rb
@@ -7,4 +7,11 @@ class HomepageTest < CapybaraTestCase
     assert page.has_content?('Welcome!')
     assert_equal 200, page.status_code
   end
+
+  def test_user_gets_error_code_for_bad_path
+    visit 'thiswontwork'
+
+    assert page.has_content?('Page not found.')
+    assert_equal 404, page.status_code
+  end
 end
